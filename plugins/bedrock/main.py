06e80e7e285c02803dbc0c2480d094b71ad9a476
warren <warren.chen830@gmail.com>
2025-04-19 17:28:47 +0800

    Add Bedrock cache configuration and related updates

--- plugins/bedrock/main.py
+++ plugins/bedrock/main.py
@@ -1,5 +1,6 @@
 from dify_plugin import Plugin, DifyPluginEnv
-
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 plugin = Plugin(DifyPluginEnv(MAX_REQUEST_TIMEOUT=120))
 
 if __name__ == '__main__':
